fac070653fb4d3dee299f1f8a4ea2892f75b8cea
Padunk <abraham.agel@gmail.com>
2020-01-21 16:40:54 +0700

    feat() : add optimist, can.js, main.js

--- bin/can.js
+++ bin/can.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
+let argv = require('optimist')
+    .usage(
+        `
+    Usage: $0 [-h] [-d directory_name]`
+    )
+    .options({
+        d: {
+            default: 'my-great-app',
+        },
+    })
+    .describe({
+        h: 'Display this help message',
+    })
+    .boolean(['h']).argv;
 
-console.log(process.argv);
+if (argv.h) {
+    showHelp();
+} else {
+    createTemplate();
+}
+
+function createTemplate() {
+    let module = require('./main');
+    module.create();
+}
+
+function showHelp() {
+    require('optimist').showHelp();
+}
