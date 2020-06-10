285d720d9a17d246e2e732f8761952aeeb8b5b01
Padunk <abraham.agel@gmail.com>
2020-06-10 07:48:49 +0700

    React template: fixing css webpack loader.

--- templates/react/webpack.config.js
+++ templates/react/webpack.config.js
@@ -3,51 +3,51 @@ const webpack = require("webpack");
 const port = 4446;
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/env"],
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(png|jpe?g|gif)/i,
-                loader: "file-loader",
-                options: {
-                    outputPath: "images",
-                },
-            },
-            {
-                test: /\.svg$/,
-                loader: "svg-inline-loader",
-            },
-        ],
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-        alias: {
-            "react-dom": "@hot-loader/react-dom",
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env"],
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "images",
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
     },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: port,
-        publicPath: "http://localhost:" + port + "/dist/",
-        hotOnly: true,
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: port,
+    publicPath: "http://localhost:" + port + "/dist/",
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
