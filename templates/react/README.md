d653ec3380397b663a7ee441b2571cd0a3710a65
Padunk <abraham.agel@gmail.com>
2021-01-05 17:21:41 +0700

    updating React and Svelte template

---
+++ templates/react/README.md
@@ -0,0 +1,3 @@
+# This is the Eject version of [create-react-app](https://create-react-app.dev/docs/getting-started/)
+
+Open it on [port: 4446](http://localhost:4446)
--- templates/react/src/App.js
+++ templates/react/src/App.js
@@ -1,9 +1,25 @@
-import * as React from "react";
-import { hot } from "react-hot-loader/root";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default hot(App);
+export default App;
