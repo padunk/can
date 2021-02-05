de4f43d309c9bc954b768b0e38eb0f7d9c41daad
Padunk <abraham.agel@gmail.com>
2021-02-05 16:54:14 +0700

    react snowpack template

---
+++ templates/react-snowpack/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello from React Snowpack</h1>
+            <p>
+                Template made by &nbsp;
+                <a href="https://anakagung.com">A.A.A</a>
+            </p>
+        </div>
+    );
+};
+
+export default App;
